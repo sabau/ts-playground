784de7b5273d3e9dbb497eecae83f02c0c0a7988
Karoly Albert Szabo <karoly.szabo@inetworking.it>
2017-07-24 16:44:30 +0200

    we want to translate this window feature in TS

--- hi.ts
+++ hi.ts
@@ -18,3 +18,42 @@ var user1: Person = {firstName: "Jane", lastName: "UJ1"};
 var user2: Student = new Student("Jane", "UJ2", "Test");
 
 document.body.innerHTML = greeter(user1) + greeter(user2);
+
+interface Window {
+    leftScreenBoundry:Function;
+    leftWindowBoundry(): number;
+}
+// Find Left Boundry of the Screen/Monitor
+let LeftWindowsBoundry = () => {
+    // Check if the window is off the primary monitor in a positive axis
+    // X,Y                  X,Y                    S = Screen, W = Window
+    // 0,0  ----------   1280,0  ----------
+    //     |          |         |  ---     |
+    //     |          |         | | W |    |
+    //     |        S |         |  ---   S |
+    //      ----------           ----------
+    if (window.leftWindowBoundry() > window.screen.width)
+    {
+        return window.leftWindowBoundry() - (window.leftWindowBoundry() - window.screen.width);
+    }
+
+    // Check if the window is off the primary monitor in a negative axis
+    // X,Y                  X,Y                    S = Screen, W = Window
+    // 0,0  ----------  -1280,0  ----------
+    //     |          |         |  ---     |
+    //     |          |         | | W |    |
+    //     |        S |         |  ---   S |
+    //      ----------           ----------
+    // This only works in Firefox at the moment due to a bug in Internet Explorer opening new windows into a negative axis
+    // However, you can move opened windows into a negative axis as a workaround
+    if (window.leftWindowBoundry() < 0 && window.leftWindowBoundry() > (window.screen.width * -1))
+    {
+        return (window.screen.width * -1);
+    }
+
+    // If neither of the above, the monitor is on the primary monitor whose's screen X should be 0
+    return 0;
+};
+window.leftScreenBoundry = LeftWindowsBoundry;
+
+window.open('https://www.google.com', 'windowName', 'resizable=1, scrollbars=1, fullscreen=0, height=200, width=650, screenX=' + (window as any).leftScreenBoundry() + ' , left=' + (window as any).leftScreenBoundry() + ', toolbar=0, menubar=0, status=1');
--- test.js
+++ test.js
@@ -1,20 +1,25 @@
-var Student = (function () {
-    function Student(firstName, middleName, lastName) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.fullName = this.firstName + " " + this.middleName + " " + this.lastName;
-    }
-    return Student;
-})();
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+/**
+ * Created by web on 7/24/17.
+ */
+function FindLeftWindowBoundry()
+{
+    // In Internet Explorer window.screenLeft is the window's left boundry
+    if (window.screenLeft)
+        return window.screenLeft;
+
+    // In Firefox window.screenX is the window's left boundry
+    if (window.screenX)
+        return window.screenX;
+
+    return 0;
 }
-var user1 = { firstName: "Jane", lastName: "UJ1" };
-var user2 = new Student("Jane", "UJ2", "Test");
-document.body.innerHTML = greeter(user1) + greeter(user2);
+
+window.leftWindowBoundry = FindLeftWindowBoundry;
+
 // Find Left Boundry of the Screen/Monitor
-var LeftWindowsBoundry = function () {
+function FindLeftScreenBoundry()
+{
+    return window.screen.width;
     // Check if the window is off the primary monitor in a positive axis
     // X,Y                  X,Y                    S = Screen, W = Window
     // 0,0  ----------   1280,0  ----------
@@ -22,9 +27,11 @@ var LeftWindowsBoundry = function () {
     //     |          |         | | W |    |
     //     |        S |         |  ---   S |
     //      ----------           ----------
-    if (window.leftWindowBoundry() > window.screen.width) {
+    if (window.leftWindowBoundry() > window.screen.width)
+    {
         return window.leftWindowBoundry() - (window.leftWindowBoundry() - window.screen.width);
     }
+
     // Check if the window is off the primary monitor in a negative axis
     // X,Y                  X,Y                    S = Screen, W = Window
     // 0,0  ----------  -1280,0  ----------
@@ -34,12 +41,19 @@ var LeftWindowsBoundry = function () {
     //      ----------           ----------
     // This only works in Firefox at the moment due to a bug in Internet Explorer opening new windows into a negative axis
     // However, you can move opened windows into a negative axis as a workaround
-    if (window.leftWindowBoundry() < 0 && window.leftWindowBoundry() > (window.screen.width * -1)) {
+    if (window.leftWindowBoundry() < 0 && window.leftWindowBoundry() > (window.screen.width * -1))
+    {
         return (window.screen.width * -1);
     }
+
     // If neither of the above, the monitor is on the primary monitor whose's screen X should be 0
     return 0;
-};
-window.leftScreenBoundry = LeftWindowsBoundry;
-window.open('https://www.google.com', 'windowName', 'resizable=1, scrollbars=1, fullscreen=0, height=200, width=650, screenX=' + window.leftScreenBoundry() + ' , left=' + window.leftScreenBoundry() + ', toolbar=0, menubar=0, status=1');
-//# sourceMappingURL=hi.js.map
+}
+
+window.leftScreenBoundry = FindLeftScreenBoundry;
+
+console.log(window.screen.width);
+console.log(window.leftScreenBoundry());
+
+console.log(window.screen.height);
+window.open('https://www.google.com', 'windowName', 'directories=no, titlebar=no, location=no, resizable=0, scrollbars=0, fullscreen=1, toolbar=0, menubar=0, status=0, screenX=' + window.leftScreenBoundry() + ' , left=' + window.leftScreenBoundry());
